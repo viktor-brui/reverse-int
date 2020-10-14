fc2aed9279787e83cce5f9824baded76c2d1c896
viktor <viktor-brui@mail.ru>
2020-10-14 17:59:17 +0300

    fix: correct code-style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 module.exports = function reverse(n) {
-    let result = 0;
-    while (n) {
-        result = result * 10 + (n % 10);
-        n = Math.floor(n / 10);
-    }
-    return result;
+  let tmp = 0, flag = 0 > n;
+  flag && (n *= -1);
+  while (n > 0) {
+    tmp *= 10;
+    tmp += n % 10;
+    n = parseInt(n / 10);
+  }
+  return tmp;
 };
